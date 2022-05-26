6d6c5d7f72d100fcc3682b20b9066b5912441e54
Mert Can Demir <validatedev@gmail.com>
2022-05-26 05:21:06 +0300

    fix: set max movie id threshold

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from src.recommend import predict_recommendations_of_user, predict_recommendations_of_movie, get_voting
 
+MAX_MOVIE_ID = 193609
+
 app = FastAPI()
 
 
@@ -12,6 +14,12 @@ async def root():
 @app.get("/recommendation/user/")
 async def recommendation_user(userId:int, numOfRecommendations:int) -> list:
     user_votings = get_voting(userId)
+
+    # Remove keys with values in user_votings where key > MAX_MOVIE_ID
+    for key in list(user_votings.keys()):
+        if key > MAX_MOVIE_ID:
+            del user_votings[key]
+
     return predict_recommendations_of_user(user_votings, numOfRecommendations)
 
 @app.get("/recommendation/movie/")
